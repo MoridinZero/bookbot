f27d9032b8798c74ff6cedef976b92cc8e0f3f28
MoridinZero <logan.garey@gmail.com>
2025-04-24 08:28:51 -0700

    Added the get_num_words function

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ def get_book_text(filepath):
     with open(filepath) as file:
         file_contents = file.read()
     return file_contents
+def get_num_words(file_contents):
+    words = file_contents.split()
+    total_words = 0
+    for word in words:
+        total_words += 1
+    return total_words
 
 def main():
     filepath = "/home/moridin_zero/workspace/github.com/moridinzero/bookbot/books/frankenstein.txt"
-    print(get_book_text(filepath))
+    print(f"{get_num_words(get_book_text(filepath))} words found in the document")
 main()
