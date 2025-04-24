91cb4d1285110c7bd1d71d7ea061bc4812a6f0d7
MoridinZero <logan.garey@gmail.com>
2025-04-24 12:25:13 -0700

    fixed the character_count function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words
+from stats import count_characters
 
 def get_book_text(filepath):
     with open(filepath) as file:
@@ -7,5 +8,7 @@ def get_book_text(filepath):
 
 def main():
     filepath = "/home/moridin_zero/workspace/github.com/moridinzero/bookbot/books/frankenstein.txt"
+    character_counts = print(count_characters(get_book_text(filepath)))
+    print(f"{character_counts}")
     print(f"{get_num_words(get_book_text(filepath))} words found in the document")
 main()
--- stats.py
+++ stats.py
@@ -4,3 +4,15 @@ def get_num_words(file_contents):
     for word in words:
         total_words += 1
     return total_words
+
+def count_characters(file_contents):
+    words = file_contents.lower()
+    count_char_dictionary = {}
+    for char in words:
+        if char not in count_char_dictionary:
+            count_char_dictionary[char] = 1
+        else:
+            count_char_dictionary[char] += 1
+    return count_char_dictionary
+
+    
