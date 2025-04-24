9a496e75467ec6089782728f9b009cc476f2c3c1
MoridinZero <logan.garey@gmail.com>
2025-04-24 14:05:26 -0700

    changed hardcoded filepath to sys.argv input

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from stats import get_num_words
 from stats import count_characters
 from stats import chars_dict_to_sorted_list
 from stats import print_report
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as file:
@@ -9,7 +10,12 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    filepath = "/home/moridin_zero/workspace/github.com/moridinzero/bookbot/books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>") 
+        sys.exit(1)
+    else:
+        filepath = sys.argv[1]
+    
     word_count = get_num_words(get_book_text(filepath))
     character_counts = count_characters(get_book_text(filepath))
     sorted_chars_list = chars_dict_to_sorted_list(count_characters(get_book_text(filepath)))
