96eebc63e018171ca3dd3848d944ec4c54a7bd87
MoridinZero <logan.garey@gmail.com>
2025-04-24 13:35:08 -0700

    added the print_report function and integrated into main

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from stats import get_num_words
 from stats import count_characters
+from stats import chars_dict_to_sorted_list
+from stats import print_report
 
 def get_book_text(filepath):
     with open(filepath) as file:
@@ -8,6 +10,8 @@ def get_book_text(filepath):
 
 def main():
     filepath = "/home/moridin_zero/workspace/github.com/moridinzero/bookbot/books/frankenstein.txt"
-    character_counts = print(count_characters(get_book_text(filepath)))
-    return character_counts
+    word_count = get_num_words(get_book_text(filepath))
+    character_counts = count_characters(get_book_text(filepath))
+    sorted_chars_list = chars_dict_to_sorted_list(count_characters(get_book_text(filepath)))
+    print_report(filepath, word_count, sorted_chars_list)
 main()
--- stats.py
+++ stats.py
@@ -15,4 +15,26 @@ def count_characters(file_contents):
             count_char_dictionary[char] += 1
     return count_char_dictionary
 
-    
+def chars_dict_to_sorted_list(char_dict):
+    sorted_chars = []
+
+    for char, count in char_dict.items():
+        result_dict = {"char": char, "num": count}
+        sorted_chars.append(result_dict)
+
+    def sort_on(dict):
+        return dict["num"]
+    sorted_chars.sort(reverse=True, key=sort_on)
+
+    return sorted_chars
+
+def print_report(path, word_count, sorted_chars_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for char_dict in sorted_chars_list:
+        if char_dict["char"].isalpha():
+            print(f"{char_dict['char']}: {char_dict['num']}")
+    print("============= END ===============")
